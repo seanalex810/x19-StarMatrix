0e1502c1434bb3f4b2d611777a7869f543c8ee03
seanalex810 <31226161+seanalex810@users.noreply.github.com>
2017-10-18 16:17:35 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<string> 
+#include<vector> 
+#include<iostream> 
+using namespace std; 
+int main() { 
+vector< vector<char> >matrix; 
+int row; 
+int col; 
+cout<< "enter a number for rows and columns"<<;  
+cin>> row 
+cin>> col
